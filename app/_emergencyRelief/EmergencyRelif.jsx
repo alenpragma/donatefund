37f8bdf9d16f3e86bb1ea0039dedc118f18ccbde
HamzaWebPro <hamza.mern2201@gmail.com>
2023-11-19 10:55:33 +0600

    first commit

--- app/_emergencyRelief/EmergencyRelif.jsx
+++ app/_emergencyRelief/EmergencyRelif.jsx
@@ -5,35 +5,35 @@ import HeadingSubheading from '../_components/_headingSubHeading/HeadingSubheadi
 const EmergencyRelif = () => {
     let data = [
         {
-            number:"22,690 ",
-            text:"Donations have been verified and still active.",
+            // number:"22,690 ",
+            text:"Caring globally, one mission at a time, fostering compassion.",
         },
         {
-            number:"0,517",
-            text:"donations have been distributed to the needy.",
+            // number:"0,517",
+            text:"Committed hearts, united for change, impacting lives through compassion.",
         },
          {
-            number:"6,450",
-            text:"Donations have been distributed to disaster-affected areas.",
+            // number:"6,450",
+            text:"Empathy in action, a journey of hope, transforming communities.",
         },
          {
-            number:"1.4 Billion",
-            text:" total funds raised so far.",
+            // number:"1.4 Billion",
+            text:"In crises, our mission thrives, spreading compassion, and creating resilience.",
         },
         {
-            number:"5,058 ",
-            text:" donations were distributed to social foundations and orphanages.",
+            // number:"5,058 ",
+            text:" Humanitarian dedication, creating ripples of kindness, changing lives forever.",
         },
         {
-            number:"4,803",
-            text:" donations have been distributed to people in emergency situations.",
+            // number:"4,803",
+            text:" Global impact, fueled by compassion, shaping a better tomorrow.",
         },
     ]
   return (
    <>
    <div className="py-[50px] md:py-[80px]">
       <Container>
-        <HeadingSubheading heading={"Help the Affected by Disasters, Shortages, and Emergency Relief."} sebHeading="HUMANITARIAN MISSION"/>
+        <HeadingSubheading heading={"A Compassionate Journey in Our Humanitarian Mission"} sebHeading="HUMANITARIAN MISSION"/>
         <div className="mt-10 flex flex-col gap-y-3 md:flex-row md:gap-x-3">
      
           <div className="w-full md:w-[49%] flex flex-wrap gap-y-4 md:gap-y-10">
